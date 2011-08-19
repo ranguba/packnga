6de213a5ce745dcbd94acb8f5dbdbeec43841b8c
Yoshihara <yoshihara@clear-code.com>
2011-08-19 17:48:48 +0900

    fix task name.

--- lib/packnga/release-task.rb
+++ lib/packnga/release-task.rb
@@ -132,7 +132,7 @@ module Packnga
     def define_reference_task
       namespace :reference do
         desc "Upload document to rubyforge."
-        task :publish => [:generate, "reference:publication:prepare"] do
+        task :publish => ["reference:generate", "reference:publication:prepare"] do
           rsync_to_rubyforge(@spec, "#{html_reference_dir}/", @spec.name)
         end
       end
