96eaae70c8e4ee493888e3d8b59dabc95479b38d
yoshihara haruka <yoshihara@clear-code.com>
2011-08-25 16:19:22 +0900

    add space.

--- lib/packnga/release-task.rb
+++ lib/packnga/release-task.rb
@@ -33,10 +33,10 @@ module Packnga
     # This attribute is path of base directory of document.
     # @param [String] path of base directory of document
     attr_writer :base_dir
-    #This attribute is message when tagging in release.
+    # This attribute is message when tagging in release.
     # @param [String] message
     attr_writer :tag_message
-    #Thes attribute is options for uploading rubyforge.
+    # This attribute is options for uploading rubyforge.
     # @param [hash] options for uploading.
     attr_writer :publish_options
     # Defines task for preparing to release.
